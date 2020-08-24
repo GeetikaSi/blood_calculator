cc53bd4ccb0e678e6bf43b9ea25b0d4eef2cbe4c
Geetika Singh <geetika.singh@duke.edu>
2020-08-24 13:05:17 -0400

    User can enter HDL test result in console & HDL processing function done

--- calculator.py
+++ calculator.py
@@ -10,4 +10,26 @@ def interface():
         elif choice == '1':
             HDL_driver()
 
+def HDL_driver():
+    #Get input
+    #Check if HDL is normal
+    #Output
+    input()
+    HDL_process()
+    
+def input():
+#input function returns a string
+    HDL_input = input("Enter your HDL value: ")
+    return int(HDL_input)
+
+def HDL_process():
+    if HDL_input >= 60:
+        return("Normal")
+    elif HDL_input >= 40 && HDL_input < 60:
+        return("Borderline Low")
+    elif HDL_input < 40:
+        return("Low")
+        
+        
 interface()
+
