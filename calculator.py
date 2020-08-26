7ffebbe0bfd4531654fb82189b3941cb73a6f545
Geetika Singh <geetika.singh@duke.edu>
2020-08-26 10:47:49 -0400

    Analyze and output HDL levels and analysis

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ def interface():
         print("Options")
         print("1 - HDL")
         print("9-Quit")
-        choice = input("Enter your choice: ")
+        choice = input("Enter your choice")
         if choice == '9':
             return
         elif choice == '1':
@@ -14,16 +14,16 @@ def HDL_driver():
     #Get input
     #Check if HDL is normal
     #Output
-    HDL_value = input()
+    HDL_value = input_HDL()
     HDL_analysis = HDL_process(HDL_value)
     output(HDL_value, HDL_analysis)
      
-def input():
+def input_HDL():
 #input function returns a string
     HDL_input = input("Enter your HDL value: ")
     return int(HDL_input)
 
-def HDL_process():
+def HDL_process(HDL_input):
     if HDL_input >= 60:
         return "Normal"
     elif 40 <= HDL_input < 60:
@@ -32,8 +32,8 @@ def HDL_process():
         return "Low"
         
 def output(test_result,analysis):
-    print("The HDL result is {}".format(test_result)
+    print("The HDL result is {}".format(test_result))
     print("That is {}".format(analysis))
-    
 
 
+interface()
