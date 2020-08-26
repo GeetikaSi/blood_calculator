9eb6d640c0178642037eb4ef756cf96878f8a33c
Geetika Singh <geetika.singh@duke.edu>
2020-08-26 11:28:34 -0400

    Analyze and display result of analysis

--- calculator.py
+++ calculator.py
@@ -2,46 +2,13 @@ def interface():
     print("My Program")
     while True:
         print("Options")
-        print("Enter '1' to check HDL value")
-        print("Enter '2' to check LDL value")
+        print("Enter '1' to check LDL value")
         print("Enter '9' to Quit")
         choice = input("Enter your choice ")
         if choice == '9':
             return
         elif choice == '1':
-            HDL_driver()
-        elif choice == '2':
             LDL_driver()
-
-def HDL_driver():
-    #Get input
-    #Check if HDL is normal
-    #Output
-    HDL_value = input_HDL()
-    HDL_analysis = HDL_process(HDL_value)
-    output(HDL_value, HDL_analysis)
-    yn = input("Do you want to check analysis for your LDL value? (y/n)")
-    if yn == 'y':
-        LDL_driver()
-    elif yn == 'n':
-        return
-     
-def input_HDL():
-#input function returns a string
-    HDL_input = input("Enter your HDL value: ")
-    return int(HDL_input)
-
-def HDL_process(HDL_input): 
-    if HDL_input >= 60:
-        return "Normal"
-    elif 40 <= HDL_input < 60:
-        return "Borderline Low"
-    elif HDL_input < 40:
-        return "Low"
-        
-def output(test_result,analysis):
-    print("The HDL result is {}".format(test_result))
-    print("That is {}".format(analysis))
 
 
 def LDL_driver():
@@ -51,11 +18,6 @@ def LDL_driver():
     LDL_value = input_LDL()
     LDL_analysis = LDL_process(LDL_value)
     output_LDL(LDL_value, LDL_analysis)
-    yn_LDL = input("Do you want to check analysis for your HDL value?(y/n)")
-    if yn_LDL == 'y':
-        HDL_driver()
-    elif yn_LDL == 'n':
-        return
      
 def input_LDL():
 #input function returns a string
